6f7feaed34fc8176046b1791be44a960bd6e33f9
Mohammedanwar Shaikh <anwarshaikh7567@gmail.com>
2018-10-07 23:48:57 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express =require('express');
+const hbs=require('hbs');
+
+
+var app=express()
+
+
+hbs.registerPartials(__dirname +'/views/partial')
+app.set('view engine','hbs');
+app.use(express.static(__dirname +'/public'));
+
+hbs.registerHelper('getCurrentDtae',()=>{
+
+    return new Date();
+});
+hbs.registerHelper('getUpper',(text)=>{
+
+    return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+    var dt=new Date().toString();
+    console.log(dt+req.url+req.method);
+    next();
+});
+
+app.get('/',(req,res)=>{
+
+res.send({
+    name:"Anwar",
+    age:22
+});
+
+})
+
+
+app.get('/about',(req,res)=>{
+
+    res.render('about.hbs',{
+        pageTitle:"About Page",
+        pageBody:"This is page related to appp info",
+        welcomeMessage:"welcome to my website"
+    });
+
+})
+
+app.listen(3000,()=>{
+
+    console.log("server is up at port 3000");
+});
